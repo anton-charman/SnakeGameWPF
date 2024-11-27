279b31ee84e4e3f82544f3bb3d4b50c7953292c4
anton-charman <antoncharman@gmail.com>
2024-11-27 21:29:16 +0000

    Added logic for drawing and moving the snake.

--- SnakeGameWPF/MainWindow.xaml.cs
+++ SnakeGameWPF/MainWindow.xaml.cs
@@ -18,18 +18,26 @@ namespace SnakeGameWPF
     public partial class MainWindow : Window
     {
         private readonly int _numSquares = 20;
+        private readonly double _squareSize;
         private readonly SolidColorBrush _evenColour = Brushes.LightGreen;
         private readonly SolidColorBrush _oddColour = Brushes.DarkGreen;
+
+        private SolidColorBrush _snakeHeadColour = Brushes.DarkBlue;
+        private SolidColorBrush _snakeBodyPartColour = Brushes.LightBlue;
+        private List<SnakeBodyPart> _snakeList = new List<SnakeBodyPart>();
+
+        public enum SnakeDirection { Left, Right, Up, Down };
+        private SnakeDirection _snakeDirection = SnakeDirection.Right;
+        private int _snakeLength;
 
         public MainWindow()
         {
             InitializeComponent();
+            _squareSize = MainArea.ActualWidth / _numSquares;
         }
 
         private void DrawMainArea()
         {
-            double squareSize = MainArea.ActualWidth / _numSquares;
-
             // Fill in the grid.
             int counter = 0;
             for(int row = 0; row < _numSquares; row++)
@@ -37,12 +45,12 @@ namespace SnakeGameWPF
                 for (int col = 0; col < _numSquares; col++)
                 {
                     Rectangle rect = new Rectangle();
-                    rect.Width = squareSize;
-                    rect.Height = squareSize;
+                    rect.Width = _squareSize;
+                    rect.Height = _squareSize;
                     rect.Fill = counter % 2 == 0 ? _evenColour : _oddColour;
 
-                    Canvas.SetTop(rect, row * squareSize);
-                    Canvas.SetLeft(rect, col * squareSize);
+                    Canvas.SetTop(rect, row * _squareSize);
+                    Canvas.SetLeft(rect, col * _squareSize);
                     MainArea.Children.Add(rect);
 
                     counter++;
@@ -59,6 +67,78 @@ namespace SnakeGameWPF
         private void Window_ContentRendered(object sender, EventArgs e)
         {
             DrawMainArea();
+        }
+
+        private void DrawSnake()
+        {
+            foreach(SnakeBodyPart snakeBodyPart in _snakeList)
+            {
+                if(snakeBodyPart.UiElement == null)
+                {
+                    snakeBodyPart.UiElement = new Rectangle
+                    {
+                        Width = _squareSize,
+                        Height = _squareSize,
+                        Fill = snakeBodyPart.IsHead ? _snakeHeadColour : _snakeBodyPartColour
+                    };
+
+                    Canvas.SetTop(snakeBodyPart.UiElement, snakeBodyPart.Position.Y);
+                    Canvas.SetLeft(snakeBodyPart.UiElement, snakeBodyPart.Position.X);
+                    MainArea.Children.Add(snakeBodyPart.UiElement);
+                }
+            }
+        }
+
+        private void DoCollisionCheck()
+        {
+
+        }
+
+        private void MoveSnake()
+        {
+            // Remove the current tail of the snake. Defined as the first element.
+            while(_snakeList.Count >= _snakeLength)
+            {
+                MainArea.Children.Remove(_snakeList[0].UiElement);
+                _snakeList.RemoveAt(0);
+            }
+
+            // Mark the remaining parts as body parts.
+            foreach(SnakeBodyPart snakeBodyPart in _snakeList)
+            {
+                snakeBodyPart.IsHead = false;
+                ((Rectangle)snakeBodyPart.UiElement).Fill = _snakeBodyPartColour;
+            }
+
+            // Expand the snake based on the current direction.
+            Point newPoint = _snakeList[_snakeList.Count - 1].Position;
+
+            switch (_snakeDirection)
+            {
+                case SnakeDirection.Left:
+                    newPoint.X -= _squareSize;
+                    break;
+                case SnakeDirection.Right:
+                    newPoint.X += _squareSize;
+                    break;
+                case SnakeDirection.Up:
+                    newPoint.Y -= _squareSize;
+                    break;
+                case SnakeDirection.Down:
+                    newPoint.Y += _squareSize;
+                    break;
+            }
+
+            // Add the new head. Defined as the last element.
+            _snakeList.Add(new SnakeBodyPart()
+            {
+                Position = newPoint,
+                IsHead = true
+            });
+
+            DrawSnake();
+
+            DoCollisionCheck();
         }
     }
 }
---
+++ SnakeGameWPF/SnakeBodyPart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeGameWPF
+{
+    public class SnakeBodyPart
+    {
+        public UIElement UiElement { get; set; }
+        public Point Position { get; set; }
+        public bool IsHead { get; set; }
+    }
+}
