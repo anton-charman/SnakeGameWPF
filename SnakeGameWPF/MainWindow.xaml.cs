45651e2b1fa2baaedbb6240752e0aa12575219e7
anton-charman <antoncharman@gmail.com>
2024-11-27 19:40:27 +0000

    Created the main window game background

--- SnakeGameWPF/MainWindow.xaml.cs
+++ SnakeGameWPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Runtime.CompilerServices;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +17,48 @@ namespace SnakeGameWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly int _numSquares = 20;
+        private readonly SolidColorBrush _evenColour = Brushes.LightGreen;
+        private readonly SolidColorBrush _oddColour = Brushes.DarkGreen;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void DrawMainArea()
+        {
+            double squareSize = MainArea.ActualWidth / _numSquares;
+
+            // Fill in the grid.
+            int counter = 0;
+            for(int row = 0; row < _numSquares; row++)
+            {
+                for (int col = 0; col < _numSquares; col++)
+                {
+                    Rectangle rect = new Rectangle();
+                    rect.Width = squareSize;
+                    rect.Height = squareSize;
+                    rect.Fill = counter % 2 == 0 ? _evenColour : _oddColour;
+
+                    Canvas.SetTop(rect, row * squareSize);
+                    Canvas.SetLeft(rect, col * squareSize);
+                    MainArea.Children.Add(rect);
+
+                    counter++;
+                }
+
+                // For correct wrap-around depending on grid dimensions.
+                if(_numSquares % 2 == 0)
+                {
+                    counter++;
+                }
+            }
+        }
+
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+            DrawMainArea();
         }
     }
 }
